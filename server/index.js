c8306a28d451023f4fee6c473ebcf6da8ba9639b
pattana pongjariya <pattana.p@gmail.com>
2025-07-24 22:43:59 +0700

    Fix: define __dirname in ESModule using fileURLToPath

--- server/index.js
+++ server/index.js
@@ -6,6 +6,11 @@ import fs from 'fs';
 import { PrismaClient } from '@prisma/client';
 import session from 'express-session';
 import bcrypt from 'bcryptjs';
+import path from 'path';
+import { fileURLToPath } from 'url';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 
 

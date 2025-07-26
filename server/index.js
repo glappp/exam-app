dc055cb7df605a9a05ae2b26e9125f529dd1f126
pattana pongjariya <pattana.p@gmail.com>
2025-07-27 00:34:58 +0700

    🔧 fix import using require for attributeDictRoute

--- server/index.js
+++ server/index.js
@@ -25,7 +25,7 @@ app.use(express.static(path.join(__dirname, '..', 'client')));
 
 // Routes
 
-import attributeDictRoute from './routes/api/attributeDictRoute.js';
+const attributeDictRoute = require('./routes/api/attributeDictRoute.js');
 app.use('/api', attributeDictRoute);
 
 
